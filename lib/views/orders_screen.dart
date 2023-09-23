7bdda6a45a47c90e922b275b27d5394b2964ca6f
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-24 00:04:36 +0300

    feat: add logo styles

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
@@ -9,7 +10,14 @@ class OrdersScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xff363936),
-        title: const Text('ЛАИМ'),
+        title: Text(
+          'ЛАИМ',
+          style: GoogleFonts.montserrat(
+            fontSize: 27,
+            fontWeight: FontWeight.w700,
+            color: const Color(0xff05FF00),
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () {},
@@ -29,3 +37,5 @@ class OrdersScreen extends StatelessWidget {
     );
   }
 }
+
+class FF00 {}
