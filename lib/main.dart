0079a41689cd5b8c68235160fc7316eb1929dce3
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-23 23:55:35 +0300

    feat: implement base OrdersScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lime/views/home_screen.dart';
+
+import '/views/views.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'ЛАИМ',
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const OrdersScreen(),
     );
   }
 }
---
+++ lib/views/orders_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff363936),
+        title: const Text('ЛАИМ'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset("assets/envelope.svg",
+                  semanticsLabel: 'Envelope')),
+        ],
+      ),
+      body: const SafeArea(child: Column()),
+    );
+  }
+}
