6e47c83294dfeb8b85e288fdab3e59d0384e2a52
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-26 11:18:23 +0300

    refactor: change contactor info screen

--- lib/views/contractor_info_screen.dart
+++ lib/views/contractor_info_screen.dart
@@ -15,14 +15,41 @@ class ContractorInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff28272d),
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: const Color(0xff363936),
-        title: const Text(
-          'Название исполнителя',
-          style: TextStyle(
-            fontSize: 27,
-            color: Color(0xffFFFFFF),
+        backgroundColor: const Color(0xff28272d),
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color(0xff333238),
+            boxShadow: const [
+              BoxShadow(
+                color: Color(
+                  0xffd64743,
+                ),
+                offset: Offset(
+                  0.0,
+                  5.0,
+                ),
+                blurRadius: 3.0,
+                spreadRadius: 0.0,
+              ), //BoxShadow
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(0.0, 0.0),
+                blurRadius: 0.0,
+                spreadRadius: 0.0,
+              ), //BoxShadow
+            ],
+          ),
+          child: const Text(
+            'Название исполнителя',
+            style: TextStyle(
+              fontSize: 27,
+              color: Color(0xffFFFFFF),
+            ),
           ),
         ),
       ),
@@ -33,12 +60,31 @@ class ContractorInfoScreen extends StatelessWidget {
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: const Color(0xff05FF00).withOpacity(0.2),
+                  color: const Color(0xff333238),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(
+                        0xffd64743,
+                      ),
+                      offset: Offset(
+                        5.0,
+                        5.0,
+                      ),
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    ), //BoxShadow
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ), //BoxShadow
+                  ],
                 ),
                 margin: const EdgeInsets.only(
                   top: 30,
-                  left: 10,
-                  right: 10,
+                  left: 20,
+                  right: 20,
                 ),
                 padding:
                     const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
@@ -53,12 +99,12 @@ class ContractorInfoScreen extends StatelessWidget {
                 child: const Text(
                   'Транспорт',
                   style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 27,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 10),
               SizedBox(
                 height: 130,
                 child: ListView(
@@ -73,12 +119,12 @@ class ContractorInfoScreen extends StatelessWidget {
                 child: const Text(
                   'Выполненые заказы',
                   style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 27,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 10),
               Container(
                 height: 390,
                 padding: const EdgeInsets.symmetric(horizontal: 35),
@@ -90,22 +136,21 @@ class ContractorInfoScreen extends StatelessWidget {
                   itemCount: orderFinishedInfo.length,
                 ),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 40),
               TextButton(
                 onPressed: onTap ?? () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      const Color(0xff05FF00).withOpacity(0.2)),
-                  padding: MaterialStateProperty.all<EdgeInsets>(
-                      const EdgeInsets.symmetric(horizontal: 50, vertical: 10)),
-                ),
                 child: const Text(
                   'Смотреть все выполненые заказы',
                   style: TextStyle(
                     fontWeight: FontWeight.w600,
                     fontSize: 15,
-                    color: Color(0xff000000),
                     decoration: TextDecoration.underline,
+                    decorationColor: Color(0xffd64743),
+                    decorationThickness: 2,
+                    color: Colors.transparent,
+                    shadows: [
+                      Shadow(offset: Offset(0, -10), color: Color(0xffdfdee4))
+                    ],
                   ),
                 ),
               ),
@@ -116,12 +161,12 @@ class ContractorInfoScreen extends StatelessWidget {
                 child: const Text(
                   'Отзывы',
                   style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 27,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
                   ),
                 ),
               ),
-              const SizedBox(height: 30),
+              const SizedBox(height: 10),
               Container(
                 height: 390,
                 padding: const EdgeInsets.symmetric(horizontal: 35),
@@ -132,7 +177,6 @@ class ContractorInfoScreen extends StatelessWidget {
                   itemCount: reviews.length,
                 ),
               ),
-              const SizedBox(height: 30),
             ],
           ),
         ),
@@ -163,8 +207,8 @@ class ContractorInfoShort extends StatelessWidget {
           child: Text(
             contractorInfoFull['name'].toString(),
             style: const TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 23,
+              fontWeight: FontWeight.w300,
+              fontSize: 20,
             ),
           ),
         ),
@@ -219,10 +263,30 @@ class FinishedOrders extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: const Color(0xff05FF00).withOpacity(0.2),
+        color: const Color(0xff333238),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(
+              0xffd64743,
+            ),
+            offset: Offset(
+              0.0,
+              5.0,
+            ),
+            blurRadius: 5.0,
+            spreadRadius: 1.0,
+          ), //BoxShadow
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(0.0, 0.0),
+            blurRadius: 0.0,
+            spreadRadius: 0.0,
+          ), //BoxShadow
+        ],
       ),
       margin: const EdgeInsets.only(
-        top: 15,
+        top: 10,
+        bottom: 10,
         left: 10,
         right: 10,
       ),
@@ -236,8 +300,8 @@ class FinishedOrders extends StatelessWidget {
             child: Text(
               orderFinishedInfo['name'].toString(),
               style: const TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 23,
+                fontWeight: FontWeight.w300,
+                fontSize: 20,
               ),
             ),
           ),
@@ -264,7 +328,7 @@ class FinishedOrders extends StatelessWidget {
                 : '',
           ),
           const SizedBox(
-            height: 10,
+            height: 20,
           ),
           InkWell(
             onTap: onTap ?? () {},
@@ -277,12 +341,15 @@ class FinishedOrders extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                   fontSize: 15,
                   decoration: TextDecoration.underline,
+                  decorationColor: Color(0xffd64743),
+                  decorationThickness: 2,
+                  color: Colors.transparent,
+                  shadows: [
+                    Shadow(offset: Offset(0, -10), color: Color(0xffdfdee4))
+                  ],
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
           ),
         ],
       ),
@@ -309,10 +376,32 @@ class ReviewItem extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: const Color(0xff05FF00).withOpacity(0.2),
+        color: const Color(0xff333238),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(
+              0xffd64743,
+            ),
+            offset: Offset(
+              0.0,
+              5.0,
+            ),
+            blurRadius: 5.0,
+            spreadRadius: 1.0,
+          ), //BoxShadow
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(0.0, 0.0),
+            blurRadius: 0.0,
+            spreadRadius: 0.0,
+          ), //BoxShadow
+        ],
       ),
       margin: const EdgeInsets.only(
-        top: 15,
+        top: 10,
+        bottom: 10,
+        left: 10,
+        right: 10,
       ),
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
       child: Column(
@@ -349,7 +438,7 @@ class ReviewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(
-            height: 10,
+            height: 20,
           ),
           InkWell(
             onTap: onTap ?? () {},
@@ -362,6 +451,12 @@ class ReviewItem extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                   fontSize: 15,
                   decoration: TextDecoration.underline,
+                  decorationColor: Color(0xffd64743),
+                  decorationThickness: 2,
+                  color: Colors.transparent,
+                  shadows: [
+                    Shadow(offset: Offset(0, -10), color: Color(0xffdfdee4))
+                  ],
                 ),
               ),
             ),
@@ -382,18 +477,20 @@ class CarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 220,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: const Color(0xffD9D9D9).withOpacity(0.2),
+    return InkWell(
+      child: Container(
+        width: 220,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xff333238),
+        ),
+        margin: const EdgeInsets.only(
+          top: 15,
+          left: 10,
+          right: 10,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
       ),
-      margin: const EdgeInsets.only(
-        top: 15,
-        left: 10,
-        right: 10,
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
     );
   }
 }
