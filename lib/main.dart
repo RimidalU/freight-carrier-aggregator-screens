85d062743f9c48e2f9e9f06d23846073284ca802
RimidalU <stankevichuuladimiru@gmail.com>
2023-09-27 14:37:08 +0300

    refactor: display home page

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
           labelStyle: TextStyle(color: Color(0xffdfdee4)),
         ),
       ),
-      home: const ClientInfoScreen(),
+      home: const HomeScreen(),
       routes: {
         // '': (context) => const HomeScreen(),
         '/orders': (context) => const OrdersScreen(),
